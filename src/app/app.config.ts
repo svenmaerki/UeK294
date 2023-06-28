90a643bb158b0e156765061a039a9cd795e4cf28
Märki Sven Ivan <s.maerki.inf21@stud.bbbaden.ch>
2023-06-28 09:27:05 +0000

    Resolve "Implementation tokenInterceptor"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,13 @@ import {
   BrowserAnimationsModule,
   provideAnimations,
 } from '@angular/platform-browser/animations';
+import { tokenInterceptor } from './auth/interceptor/token.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
     provideRouter(routes),
     provideAnimations(),
     provideAnimations(),
