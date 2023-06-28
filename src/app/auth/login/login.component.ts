a99c48642c5317a013b62742f95a8af1bc8581cc
Märki Sven Ivan <s.maerki.inf21@stud.bbbaden.ch>
2023-06-28 09:42:27 +0000

    Resolve "Implementation Login Komponente"

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../service/auth.service';
+import { Constants } from '../../data/constants';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  protected readonly Constants = Constants;
+  protected form: FormGroup = new FormGroup({});
 
+  protected authService = inject(AuthService);
+  private fb = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      username: ['user', Validators.required],
+      password: ['12345', Validators.required],
+    });
+  }
+
+  login() {
+    if (!this.form?.valid) return;
+
+    const { username, password } = this.form.value;
+    this.authService
+      .login({ username, password }, Constants.routes.todoList)
+      .subscribe();
+  }
 }
